f0bc89659b4ba1af11fc39436a6e468cf2ccce07
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-04 19:43:51 -0300

    projeto angular de crud pessoa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from "./component/menu/menu";
+import { Formulario } from "./component/formulario/formulario";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, Formulario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
